168e7dbb82226e47b4d749562756443cabb63034
Constantin Heinzler <constantin.heinzler@glueckkanja.com>
2025-04-14 14:11:32 +0200

    Remove unneccessary logging

--- SCEPmanClient/Private/x509/Save-CertificateToStore.ps1
+++ SCEPmanClient/Private/x509/Save-CertificateToStore.ps1
@@ -53,10 +53,8 @@ Function Save-CertificateToStore {
     $CertificateCollection.Import($Certificate.RawData, $null, $KeyStorageFlag)
 
     If ($PrivateKey.SignatureAlgorithm -in ('RSA', 'http://www.w3.org/2000/09/xmldsig#rsa-sha1')) {
-        Write-Verbose "$($MyInvocation.MyCommand): Merging certificate with RSA private key"
         $MergedCertificate = [System.Security.Cryptography.X509Certificates.RSACertificateExtensions]::CopyWithPrivateKey($CertificateCollection[0], $PrivateKey)
     } ElseIf ($PrivateKey.SignatureAlgorithm -eq 'ECDSA') {
-        Write-Verbose "$($MyInvocation.MyCommand): Merging certificate with ECDSA private key"
         $MergedCertificate = [System.Security.Cryptography.X509Certificates.ECDsaCertificateExtensions]::CopyWithPrivateKey($CertificateCollection[0], $PrivateKey)
     } Else {
         throw "Unsupported signature algorithm $($PrivateKey.SignatureAlgorithm)"
@@ -64,7 +62,6 @@ Function Save-CertificateToStore {
 
     $TemporaryPassword = New-RandomPassword
 
-    Write-Verbose "$($MyInvocation.MyCommand): Exporting certificate and private key to PFX bundle"
     $PfxBundle = $MergedCertificate.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Pkcs12, $TemporaryPassword)
 
     If ($PSBoundParameters.ContainsKey('Exportable')) {
@@ -75,7 +72,6 @@ Function Save-CertificateToStore {
 
     $IssuedCertificateAndKey = [System.Security.Cryptography.X509Certificates.X509Certificate2]::new($PfxBundle, $TemporaryPassword, $KeyStorageFlags)
 
-    Write-Verbose "$($MyInvocation.MyCommand): Opening certificate store at $StorageLocation"
     $CertificateStore = [System.Security.Cryptography.X509Certificates.X509Store]::new("My", $StorageLocation)
     $CertificateStore.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadWrite -bor [System.Security.Cryptography.X509Certificates.OpenFlags]::OpenExistingOnly)
 
