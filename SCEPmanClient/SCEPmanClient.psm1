7849271d1886ca43a5b3add3ede91dd3a9b9b51a
Constantin Heinzler <constantin.heinzler@glueckkanja.com>
2025-08-22 14:40:04 +0200

    Remove platform specific script loading

--- SCEPmanClient/SCEPmanClient.psm1
+++ SCEPmanClient/SCEPmanClient.psm1
@@ -1,15 +1,8 @@
 $PrivateScripts = @(Get-ChildItem $PSScriptRoot\Private\*.ps1 -Recurse)
 $PublicScripts = @(Get-ChildItem $PSScriptRoot\Public\*.ps1 -Recurse)
 
-$ScriptsToSource = $PrivateScripts + $PublicScripts
-
-If($PSVersionTable.PSEdition -eq 'Core') {
-    $PlatformSpecificScripts = @(Get-ChildItem $PSScriptRoot\PlatformSpecific\PowerShellCore\*.ps1 -Recurse)
-    $ScriptsToSource += $PlatformSpecificScripts
-}
-
 # Dot source all scripts
-Foreach ($Script in $ScriptsToSource)
+Foreach ($Script in ($PrivateScripts + $PublicScripts))
 {
     . $Script.FullName
 }
